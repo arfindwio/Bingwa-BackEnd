12ac6bbfe0946ff27953026984e75c31e4f71006
Arfin Dwi Octavianto <arfindwioctavianto@gmail.com>
2024-01-11 13:24:17 +0700

    feat: add endpoint getTrackingByCourseId

--- routes/enrollment.routes.js
+++ routes/enrollment.routes.js
@@ -1,10 +1,10 @@
 const router = require("express").Router();
-const { getAllEnrollment, getDetailEnrollment, courseEnrollment } = require("../controllers/enrollment.controllers");
+const { getAllEnrollment, getEnrollmentByCourseId, courseEnrollment } = require("../controllers/enrollment.controllers");
 const Auth = require("../middlewares/authentication");
 const checkRole = require("../middlewares/checkRole");
 
 router.get("/", Auth, checkRole(["user", "admin"]), getAllEnrollment);
-router.get("/:id", Auth, checkRole(["user", "admin"]), getDetailEnrollment);
+router.get("/:courseId", Auth, checkRole(["user", "admin"]), getEnrollmentByCourseId);
 router.post("/:courseId", Auth, checkRole(["user", "admin"]), courseEnrollment);
 
 module.exports = router;
